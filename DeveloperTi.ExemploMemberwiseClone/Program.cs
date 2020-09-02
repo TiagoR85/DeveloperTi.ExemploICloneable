4130f690db6a46b43b0b129bf9af4fd35b3c1605
Tiago Rodrigues <tiago.rds85@hotmail.com>
2020-09-02 15:38:32 -0300

    Alteração para melhor compreensão

--- DeveloperTi.ExemploMemberwiseClone/Program.cs
+++ DeveloperTi.ExemploMemberwiseClone/Program.cs
@@ -14,25 +14,36 @@ namespace DeveloperTi.ExemploMemberwiseClone
             };
 
             var livro2 = livro1.ShallowCopy();
+            Console.WriteLine("ShallowCopy em Livro 1, gerando um clone, Livro 2.");
+            Console.WriteLine("ShallowCopy, copia os typesValues, mas copiando também, o endereço de memória dos referenceValue.");
+            Console.WriteLine("");
+            Console.WriteLine("Livro 1 - Autor:{0}, Titulo:{1}, Categoria:{2}", livro1.Author, livro1.Title, livro1.Category.Name);
+            Console.WriteLine("Livro 2 - Autor:{0}, Titulo:{1}, Categoria:{2}", livro2.Author, livro2.Title, livro2.Category.Name);
 
+            Console.WriteLine("");
+            Console.WriteLine("Alterando Livro 2...");
+            Console.WriteLine("");
+
+            livro2.Author = "Junior";
+            livro2.Title = "Coloca nas mãos de Deus";
+            livro2.Category.Name = "AutoAjuda";
             Console.WriteLine("Livro 1 - Autor:{0}, Titulo:{1}, Categoria:{2}", livro1.Author, livro1.Title, livro1.Category.Name);
             Console.WriteLine("Livro 2 - Autor:{0}, Titulo:{1}, Categoria:{2}", livro2.Author, livro2.Title, livro2.Category.Name);
             Console.WriteLine("");
-
-            livro1.Author = "Junior";
-            livro1.Title = "Será que deu certo??";
-            livro1.Category.Name = "AutoAjuda";
-            Console.WriteLine("Livro 1 - Autor:{0}, Titulo:{1}, Categoria:{2}", livro1.Author, livro1.Title, livro1.Category.Name);
-            Console.WriteLine("Livro 2 - Autor:{0}, Titulo:{1}, Categoria:{2}", livro2.Author, livro2.Title, livro2.Category.Name);
+            Console.WriteLine("Perceba que após alterar as propriedades de Livro 2, " +
+                "{0}ele tbm alterou o conteúdo da referenceValue Categoria, {0}tanto em Livro 1, como em Livro 2", Environment.NewLine);
             Console.WriteLine("");
 
             var livro3 = livro1.DeepCopy();
-
-            livro1.Author = "Otávio";
-            livro1.Title = "Será que deu certo 2??";
-            livro1.Category.Name = "SelfHeal";
+            Console.WriteLine("DeepCopy em Livro 3");
+            Console.WriteLine("Alterando Livro 3...");
+            Console.WriteLine("");
+            livro3.Author = "Otávio";
+            livro3.Title = "Cada um tem sua cruz";
+            livro3.Category.Name = "SelfHeal";
             Console.WriteLine("Livro 1 - Autor:{0}, Titulo:{1}, Categoria:{2}", livro1.Author, livro1.Title, livro1.Category.Name);
             Console.WriteLine("Livro 3 - Autor:{0}, Titulo:{1}, Categoria:{2}", livro3.Author, livro3.Title, livro3.Category.Name);
+            Console.WriteLine("DeepCopy, copia os typesValues, e cria novos espaços na memória para armazenar os referenceTypes");
             Console.ReadKey();
         }
     }
