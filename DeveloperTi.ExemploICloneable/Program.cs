490fe5731f681c18202b803caaf326bb26ae34f7
Tiago Rodrigues <tiago.rds85@hotmail.com>
2020-09-02 14:44:46 -0300

    Adicionar arquivos de projeto.

---
+++ DeveloperTi.ExemploICloneable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeveloperTi.ExemploICloneable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
